66eea0f590be49459b3a0df1f5c3b7176fdddc36
Josh W Lewis <josh.w.lewis@gmail.com>
2017-11-04 23:18:44 -0500

    Better output layout

---
+++ output/light/blink/colors/base16-unikitty-light.js
@@ -0,0 +1,47 @@
+// Base16 Unikitty Light
+// Scheme: Josh W Lewis (@joshwlewis)
+
+base00 = '#ffffff';
+base01 = '#e1e1e2';
+base02 = '#c4c3c5';
+base03 = '#a7a5a8';
+base04 = '#89878b';
+base05 = '#6c696e';
+base06 = '#4f4b51';
+base07 = '#322d34';
+base08 = '#d8137f';
+base09 = '#d65407';
+base0A = '#dc8a0e';
+base0B = '#17ad98';
+base0C = '#149bda';
+base0D = '#775dff';
+base0E = '#aa17e6';
+base0F = '#e013d0';
+
+t.prefs_.set('color-palette-overrides', 
+                        [base00,
+                        base08,
+                        base0B,
+                        base0A,
+                        base0D,
+                        base0E,
+                        base0C,
+                        base05,
+                        base03,
+                        base08,
+                        base0B,
+                        base0A,
+                        base0D,
+                        base0E,
+                        base0C,
+                        base07,
+                        base09,
+                        base0F,
+                        base01,
+                        base02,
+                        base04,
+                        base06]);
+
+t.prefs_.set('cursor-color', "rgba(108, 105, 110, 0.5)");
+t.prefs_.set('foreground-color', color_scheme.base05);
+t.prefs_.set('background-color', color_scheme.base00);
---
+++ output/light/vis/themes/base16-unikitty-light.lua
@@ -0,0 +1,63 @@
+-- base16-vis (https://github.com/pshevtsov/base16-vis)
+-- by Petr Shevtsov
+-- Unikitty Light scheme by Josh W Lewis (@joshwlewis)
+
+local lexers = vis.lexers
+
+local colors = {
+	['base00'] = '#ffffff',
+	['base01'] = '#e1e1e2',
+	['base02'] = '#c4c3c5',
+	['base03'] = '#a7a5a8',
+	['base04'] = '#89878b',
+	['base05'] = '#6c696e',
+	['base06'] = '#4f4b51',
+	['base07'] = '#322d34',
+	['base08'] = '#d8137f',
+	['base09'] = '#d65407',
+	['base0A'] = '#dc8a0e',
+	['base0B'] = '#17ad98',
+	['base0C'] = '#149bda',
+	['base0D'] = '#775dff',
+	['base0E'] = '#aa17e6',
+	['base0F'] = '#e013d0',
+}
+
+lexers.colors = colors
+
+local fg = ',fore:'..colors.base05..','
+local bg = ',back:'..colors.base00..','
+
+lexers.STYLE_DEFAULT = bg..fg
+lexers.STYLE_NOTHING = bg
+lexers.STYLE_CLASS = 'fore:'..colors.base0A
+lexers.STYLE_COMMENT = 'fore:'..colors.base03..',italics'
+lexers.STYLE_CONSTANT = 'fore:'..colors.base09
+lexers.STYLE_DEFINITION = 'fore:'..colors.base0E
+lexers.STYLE_ERROR = 'fore:'..colors.base08..',italics'
+lexers.STYLE_FUNCTION = 'fore:'..colors.base0D
+lexers.STYLE_KEYWORD = 'fore:'..colors.base0E
+lexers.STYLE_LABEL = 'fore:'..colors.base0A
+lexers.STYLE_NUMBER = 'fore:'..colors.base09
+lexers.STYLE_OPERATOR = 'fore:'..colors.base05
+lexers.STYLE_REGEX = 'fore:'..colors.base0C
+lexers.STYLE_STRING = 'fore:'..colors.base0B
+lexers.STYLE_PREPROCESSOR = 'fore:'..colors.base0A
+lexers.STYLE_TAG = 'fore:'..colors.base0A
+lexers.STYLE_TYPE = 'fore:'..colors.base0A
+lexers.STYLE_VARIABLE = 'fore:'..colors.base0D
+lexers.STYLE_WHITESPACE = 'fore:'..colors.base02
+lexers.STYLE_EMBEDDED = 'fore:'..colors.base0F
+lexers.STYLE_IDENTIFIER = 'fore:'..colors.base08
+
+lexers.STYLE_LINENUMBER = 'fore:'..colors.base02..',back:'..colors.base00
+lexers.STYLE_CURSOR = 'fore:'..colors.base00..',back:'..colors.base05
+lexers.STYLE_CURSOR_PRIMARY = 'fore:'..colors.base00..',back:'..colors.base05
+lexers.STYLE_CURSOR_LINE = 'back:'..colors.base01
+lexers.STYLE_COLOR_COLUMN = 'back:'..colors.base01
+lexers.STYLE_SELECTION = 'back:'..colors.base02
+lexers.STYLE_STATUS = 'fore:'..colors.base04..',back:'..colors.base01
+lexers.STYLE_STATUS_FOCUSED = 'fore:'..colors.base09..',back:'..colors.base01
+lexers.STYLE_SEPARATOR = lexers.STYLE_DEFAULT
+lexers.STYLE_INFO = 'fore:default,back:default,bold'
+lexers.STYLE_EOF = ''
